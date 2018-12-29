a6b0b878e974e64d5f9e27f9e8d48382d4627b1c
初煌 方 <258700581@qq.com>
2018-12-30 02:16:22 +0800

    升级boostrap 4

---
+++ Web.Mvc/wwwroot/js/siteInit.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    $(function () {
+        var url = window.location.href;
+        $('ul.navbar-nav li a').filter(function () {
+            return url.indexOf(this.href) !== -1;
+        }).parent().addClass('active');
+    });
+})();
