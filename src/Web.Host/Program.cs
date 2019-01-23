505778bab0a83598a8d769d9d2ed04bc8b63ab35
Snopx <258700581@qq.com>
2019-01-23 11:20:53 +0800

    api项目修改

---
+++ src/Web.Host/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Host
+{
+    /// <summary>
+    /// Program to setup
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// program entry
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        /// <summary>
+        /// IWebHostBuilder builder method
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ src/Web.Host/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Web.Host
+{
+    /// <summary>
+    /// Startup
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// construction of Startup
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        /// <summary>
+        /// Get the Configuration (DI)
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+
+        /// <summary>
+        /// add service to collection
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            #region Swagger
+            services.AddSwaggerGen(o =>
+            {
+                o.SwaggerDoc("v1", new Info
+                {
+                    Version = "v0.1.0",
+                    Title = "DF's Blog API",
+                    Description = "框架说明文档",
+                    Contact = new Contact { Name = "DarrenFang", Email = "darrenfang94@gmail.com", Url = "https://github.com/Snopx" }
+                });
+                string basePath = Microsoft.DotNet.PlatformAbstractions.ApplicationEnvironment.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "Web.Host.xml");
+                o.IncludeXmlComments(xmlPath);
+            });
+            #endregion
+        }
+
+
+        /// <summary>
+        /// configure the http pipeline
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            #region Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api V1");
+                c.RoutePrefix = "swagger";
+            });
+            #endregion
+
+            app.UseMvc();
+        }
+    }
+}
